45a01472f8c2b9b0c8782d88bf149d42079abd9a
ahamedsahib <adsahib39@gmail.com>
2021-07-12 15:58:22 +0530

    [Ahamed]Remove wordfrom Hash Table

--- HashTable/MyMapNode.cs
+++ HashTable/MyMapNode.cs
@@ -99,6 +99,32 @@ namespace HashTable
             }
             return default(V);
         }
+        public void Remove(K Key)
+        {
+            int pos = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> list = GetLinkedList(pos);
+            bool found = false;
+            KeyValue<K, V> keyVal = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> i in list)
+            {
+                if (i.Key.Equals(Key))
+                {
+                    found = true;
+                    keyVal = i;
+                    break;
+                }
+            }
+            if (found)
+            {
+                list.Remove(keyVal);
+                Console.WriteLine("Succesfully Removed ");
+
+            }
+            else
+            {
+                Console.WriteLine("Not Found");
+            }
+        }
 
     }
 }
