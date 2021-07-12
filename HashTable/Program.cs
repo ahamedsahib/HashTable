972121456d4b9e1e587665fa436cdf9b4abb79f9
ahamedsahib <adsahib39@gmail.com>
2021-07-12 15:34:49 +0530

    [Ahamed]Finding Frequency of Words

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HashTable
 {
@@ -7,6 +8,35 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Hash Table Implementation Program");
+
+            string str = "To be or not to be".ToLower();
+            //converting string to array
+            string[] arr = str.Split();
+
+            //set for getting unique elements in array
+            HashSet<string> set = new HashSet<string>(arr);
+            int count;
+            //object for mymap class
+            MyMapNode<string, int> map = new MyMapNode<string, int>(arr.Length);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count = map.Check(arr[i]);
+                if (count > 1)
+                {
+                    map.Add(arr[i], count);
+                }
+                else
+                {
+
+                    map.Add(arr[i], 1);
+                }
+            }
+
+            //printing values with frequency
+            foreach (var i in set)
+            {
+                Console.WriteLine($"{i}---> {map.Get(i)}");
+            }
         }
     }
 }
